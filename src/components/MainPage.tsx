80aef3f135002d3c5c91f5613d2c8ad814ff52ec
alhadycompanyforlaptops-ops <alhadycompanyforlaptops@gmail.com>
2025-08-03 04:02:36 +0300

    Fix corrupted MainPage component

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,1 +1,26 @@
-{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
+import React from 'react';
+import SearchSection from './SearchSection';
+import StatsSection from './StatsSection';
+import AllResultsSection from './AllResultsSection';
+import ExamSchedule from './ExamSchedule';
+
+interface MainPageProps {
+  onNavigate: (page: string) => void;
+}
+
+const MainPage: React.FC<MainPageProps> = ({ onNavigate }) => {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <main className="container mx-auto px-4 py-8">
+        <div className="space-y-8">
+          <SearchSection />
+          <StatsSection />
+          <AllResultsSection />
+          <ExamSchedule />
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default MainPage;
